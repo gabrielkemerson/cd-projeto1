1e05ebf21b7d19fbbb51a72d5ed6b2c1cc494b00
gabrielkemerson <gabrielkemersony@gmail.com>
2023-07-24 13:44:19 -0300

    Adicionando informações a "Details"

--- recipes/urls.py
+++ recipes/urls.py
@@ -3,6 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path("", views.Home),
-    path("recipes/<int:id>/", views.Recipe),
+    path("", views.home),
+    path("recipes/<int:id>/", views.recipe),
 ]
--- recipes/views.py
+++ recipes/views.py
@@ -1,24 +1,16 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 
-def Home(request):
+
+def home(request):
     return render(request, 'recipes/pages/home.html', context={
         'recipes': [make_recipe() for _ in range(10)],
     })
 
-def Recipe(request, id):
+
+def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'recipe': make_recipe(),
+        'name': 'Gabriel Kemerson',
+        'contador': make_recipe(),
+        'is_detail_page': True,
     })
-
-
-
-
-
-
-
-
-
-
-
-
