2632010414ceac13e4a2011432f9f29dee36e415
gabrielkemerson <gabrielkemersony@gmail.com>
2023-07-28 14:25:23 -0300

    Atualização de models(Recipes), e relacionamento

--- recipes/models.py
+++ recipes/models.py
@@ -1,27 +1,51 @@
+# LEMBRE-SE !! após qualquer alteração ou criação você deve informarpro django que essa alteração foi feita para que ele possa deixar a sua base de dados igual ao seu código. Para isso crie a migração através do comando 'python manage.py makemigrations' e em seguida realize essas migrações para o banco    de dados atravéz do comando 'python manage.py migrate'
+
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+# Create your models here.  
 
-class Recipe(models.Model):
+class Category(models.Model):
+
+    name = models.CharField(max_length=65)
+
+
+class Recipe(models.Model): 
+
     # Cria uma coluna chamada 'title' que recebe um CharField(que é o equivalente ao VarChar no SQL) que pode receber no máximo 65 caracteres
     title = models.CharField(max_length=65)
+
     # Cria uma coluna chamada description que também recebe um Charfield de no máximo 200 caracteres
     description = models.CharField(max_length=200)
+
     # Basicamente é usado como identificador único como se fosse uma Primary Key
     slug = models.SlugField()
+
     # Cria uma coluna chamada preparation_time que recebe um IntegerField, que é um objeto que recebe valores Inteiros    
     preparation_time = models.IntegerField()
     preparation_time_unit = models.CharField(max_length=65)
     servings = models.IntegerField()
     servings_unit = models.CharField(max_length=65)
+
     # Cria uma coluna que recebe um objeto de texto que é diferente de um input como um CharField por exemplo, e neste caso o TextField não tem limite de caracteres
     preparation_steps = models.TextField()
+
     # Cria uma coluna chamada 'preparation_steps_is_html que vai receber um objeto que recebe valores boleanos
     preparation_steps_is_html = models.BooleanField(default=False)
-    # Cria uma coluna do tipo DATA e o parametro 'auto_now_add=True' faz com que esta coluna pegue a data atual e adicione neste campo automaticamente
+
+    # Cria uma coluna do tipo DATA e o parametro 'auto_now_add=True' faz com que esta coluna pegue a data atual e adicione neste campo automaticamente 
     created_at = models.DateTimeField(auto_now_add=True)
+
     # Cria uma coluna do tipo data que vai adicionar a data da ultima alteração, a diferença deste elemento para o elemento acima é que quando a receita for atualizada apenas esta data será alterada, diferente da data de criação
     updated_at = models.DateTimeField(auto_now=True)
     is_published = models.BooleanField(default=False)
+
     # Cria uma coluna que vai receber uma imagem. O parametro 'upload_to' recebe uma pasta e uma data que vai ser a data em que aquela imagem foi adicionada
     cover = models.ImageField(upload_to='recipes/covers/%y/%m/%d')
+
+    # Aqui é criada uma chave estrangeira, que servirá para relacionar as duas tabelas Recipe e Category. Os parametros deste elemento são (O primeiro indica de qual outra classe vem a relação, o segundo faz com que caso essa categoria seja excluida automaticamente será atribuido o valor NULL para este elemento para que não haja inconsistência nos dados, e o terceiro parâmetro permite com que essa ação seja feita)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True)
+
+    # Aqui é criada uma chave estrangeira para o autor da receita, com as mesmas caracteristicas do elemento anterior, porém neste caso não precisaremos criar uma classe de usuário manualmente, pois fizemos isso apartir de um import do django que já disponibiliza uma classe User pré criada para nós
+    author = models.ForeignKey(User, on_delete=models.SET_NULL, null=True)
+ 
