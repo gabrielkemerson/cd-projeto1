620fb2057cfb9f4de02652d3bc126541baf0a720
gabrielkemerson <gabrielkemersony@gmail.com>
2023-08-22 23:45:29 -0300

    Alteração views.py e category.html

--- recipes/views.py
+++ recipes/views.py
@@ -9,15 +9,17 @@ def home(request):
         'recipes': recipes,
     })
 
+
 def category(request, category_id):
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
+        'title': f'{recipes.first().category.name}  - Category'
     })
+
 
 def recipe(request, id):
     return render(request, 'recipes/pages/recipe-view.html', context={
-        'name': 'Gabriel Kemerson',
         'contador': make_recipe(),
         'is_detail_page': True,
     })
