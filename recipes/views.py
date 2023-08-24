783805a52034270a8ee71c4ca88d7fffac34a400
gabrielkemerson <gabrielkemersony@gmail.com>
2023-08-24 14:54:25 -0300

    Tratando erros da categoria

--- recipes/views.py
+++ recipes/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from utils.recipes.factory import make_recipe
 from . models import Recipe
-
+from django.http import HttpResponse
 
 def home(request):
     recipes = Recipe.objects.filter(is_published=True).order_by('-id')
@@ -11,7 +11,12 @@ def home(request):
 
 
 def category(request, category_id):
+    # recipes recebe todos os objetos de Recipe filtrados por(id e que estejam publicados) os objetos serão ordenados por ordem decrescente dos ID
     recipes = Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id')
+    
+    if not recipes:
+        return HttpResponse(content='Not foun page', status='404')
+
     return render(request, 'recipes/pages/category.html', context={
         'recipes': recipes,
         'title': f'{recipes.first().category.name}  - Category'
