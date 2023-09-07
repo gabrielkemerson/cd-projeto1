8675fd7656950408b902c84de0c9f53599f390f7
gabrielkemerson <gabrielkemersony@gmail.com>
2023-09-07 17:44:12 -0300

    Comentários do test_recipe_view.py

--- recipes/tests/test_recipe_view.py
+++ recipes/tests/test_recipe_view.py
@@ -1,15 +1,21 @@
+# O testCase já é importado por padrão no test.py do app
 from django.test import TestCase
+# O reverse retorna a URL apartir de uma view
+# Já a função resolve serve para testar se uma URL específica está associada à visualização correta    # noqa
 from django.urls import reverse, resolve
+# Lembre-se sempre de importar as views para testa-las
 from recipes import views
 
 
+# Esta é a classe que contem todos os testes deste arquivo
 class RecipeViewsTest(TestCase):
-
+    # Lembre-se de sempre de começar o nome dos seus testes com a palavra "test" e escreva o nome dos testes de maneira bem detalhada, não importa se eles vão ficar muito grandes, o importante é ser bem descritivo    # noqa
     def test_recipe_home_view_function_is_correct(self):
         view = resolve(reverse('recipes:home'))
         self.assertIs(view.func, views.home)
 
     def test_recipe_category_view_function_is_correct(self):
+        # Quando for usar o reverse, lembre-se de que, se a url que você está passando recebe algum parâmetro você deve passar este parâmetro. Neste caso usamos o "kwargs" para isso    # noqa
         view = resolve(reverse('recipes:category', kwargs={'category_id': 1}))
         self.assertIs(view.func, views.category)
 
