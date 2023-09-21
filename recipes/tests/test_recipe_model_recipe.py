da04829308f0017ec0579dc04aadaa84f9a248ed
gabrielkemerson <gabrielkemersony@gmail.com>
2023-09-21 11:34:29 -0300

    Testando restrição de caracteres com parameterized

--- recipes/tests/test_recipe_model_recipe.py
+++ recipes/tests/test_recipe_model_recipe.py
@@ -1,5 +1,6 @@
 from .test_recipe_base import RecipeTestBase
 from django.core.exceptions import ValidationError
+from parameterized import parameterized
 
 
 # esta classe foi feita para testar os models do app recipes
@@ -10,30 +11,20 @@ class RecipeModelTest(RecipeTestBase):
         self.recipe = self.make_recipe()
         return super().setUp()
 
-    # Aqui é testado se o djando realmente está validando a quantidade
-    # de caracteres do titulo
-    def test_recipe_title_rises_error_if_title_has_more_than_65_chars(self):
-        self.recipe.title = 'A' * 70
-        # Criamos um bloco com with e nele fazemos uma asserção afirmando
-        # que o Django irá levantar um erro de validação
+    # Aqui é usado o parameterized para Fazer um sub grupo de testes dentro do teste    # noqa
+    # Os parametros devem ser passados no modelo a baixo, com uma lista de tuplas   # noqa
+    @parameterized.expand([
+        ('title', 65),
+        ('description', 200),
+        ('preparation_time_unit', 65),
+        ('servings_unit', 65),
+    ])
+    # Na função a baixo são definidos os dois campos que irão representar cada elemento da tupla que serão "field" e "max_length" respectivamente   # noqa
+    def test_recipe_fields_max_length(self, field, max_length):
+        # Aqui é usado o setattr() para passar valores para objeto já criado pela função setUp o titulo recebe propositalmente uma quantidade de caracteres além da quantidade permitida para que um erro seja levantado    # noqa
+        setattr(self.recipe, field, 'A' * (max_length + 1))
+
+        # Aqui é feita uma asserção afirmando que um erro de validação será levantado    # noqa
         with self.assertRaises(ValidationError):
-            # Infelizmente o Django não faz a validação por padrão
-            # por isso temos que usar o "full_clean()" para garantir
-            # que o Django valide as informações de acordo com os padrões
-            # dos Models criados
+            # Aqui é feito a checagem através do full_clean() para que o erro seja levantado já que infelizmente o Django não faz a validação poir padrão   # noqa
             self.recipe.full_clean()
-
-    def test_recipe_fields_max_length(self):
-
-        fields = [
-            ('title', 65),
-            ('description', 200),
-            ('preparation_time_unit', 65),
-            ('servings_unit', 65),
-        ]
-
-        for field, max_length in fields:
-            setattr(self.recipe, field, 'A' * (max_length + 1))
-
-            with self.assertRaises(ValidationError):
-                self.recipe.full_clean()
