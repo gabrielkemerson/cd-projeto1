9889607e428ff443ad302f6fdc2be4c8f3218081
gabrielkemerson <gabrielkemersony@gmail.com>
2023-10-13 18:31:20 -0300

    Incluindo paginação na view home parte 1

--- recipes/views.py
+++ recipes/views.py
@@ -7,14 +7,25 @@ from . models import Recipe
 from django.http import Http404
 # Este import é para que o django consiga usar um OR em uma busca por titulo ou descrição que estejam contidos em uma receita   # noqa
 from django.db.models import Q
+from django.core.paginator import Paginator
 
 
 def home(request):
 
-    recipes = Recipe.objects.filter(is_published=True).order_by('-id')
+    recipes = Recipe.objects.filter(
+        is_published=True,
+    ).order_by('-id')
+
+    # A variável recebe uma query string chamada 'page' caso nela não exista nada retornara o valor 1 # noqa
+    current_page = request.GET.get('page', 1)
+    # A variável recebe um objeto paginator que irá retornar os objetos 'recipes' de acordo com a quantidade definida no segundo parâmetro # noqa
+    paginator = Paginator(recipes, 6)
+    # A variável recebe o objeto que irá retornar uma página, como no parâmetro está sendo passado o 'current_page' a página exibida será determinada pelo valor da query string nele atribuida # noqa
+    page_obj = paginator.get_page(current_page)
 
     return render(request, 'recipes/pages/home.html', context={
-        'recipes': recipes,
+        # Aqui é atribuido ao valor 'recipes' o page_obj que irá retornar as páginas de acordo com a regra de paginação definida a cima # noqa
+        'recipes': page_obj,
     })
 
 
