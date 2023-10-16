fb9ecea8bfb1fc258c5f6be77217b8c5d70f8124
gabrielkemerson <gabrielkemersony@gmail.com>
2023-10-16 20:32:49 -0300

    incluindo paginação em category e search

--- recipes/views.py
+++ recipes/views.py
@@ -7,8 +7,7 @@ from . models import Recipe
 from django.http import Http404
 # Este import é para que o django consiga usar um OR em uma busca por titulo ou descrição que estejam contidos em uma receita   # noqa
 from django.db.models import Q
-from django.core.paginator import Paginator
-from utils.pagination import make_pagination_range
+from utils.pagination import make_pagination
 
 
 def home(request):
@@ -17,21 +16,7 @@ def home(request):
         is_published=True,
     ).order_by('-id')
 
-    # A variável recebe uma query string chamada 'page' caso nela não exista nada retornara o valor 1 # noqa
-    try:
-        current_page = int(request.GET.get('page', 1))
-    except ValueError:
-        current_page = 1
-    # A variável recebe um objeto paginator que irá retornar os objetos 'recipes' de acordo com a quantidade definida no segundo parâmetro # noqa
-    paginator = Paginator(recipes, 2)
-    # A variável recebe o objeto que irá retornar uma página, como no parâmetro está sendo passado o 'current_page' a página exibida será determinada pelo valor da query string nele atribuida # noqa
-    page_obj = paginator.get_page(current_page)
-
-    pagination_range = make_pagination_range(
-        paginator.page_range,
-        4,
-        current_page
-    )
+    page_obj, pagination_range = make_pagination(request, recipes, 6)
 
     return render(request, 'recipes/pages/home.html', context={
         # Aqui é atribuido ao valor 'recipes' o page_obj que irá retornar as páginas de acordo com a regra de paginação definida a cima # noqa
@@ -43,9 +28,11 @@ def home(request):
 def category(request, category_id):
     # recipes recebe todos os objetos de Recipe filtrados por(que tenham o mesmo id de category, e que estejam publicados) os objetos serão ordenados por ordem decrescente dos ID    # noqa
     recipes = get_list_or_404(Recipe.objects.filter(category__id=category_id, is_published=True).order_by('-id'))    # noqa
+    page_obj, pagination_range = make_pagination(request, recipes, 3)
 
     return render(request, 'recipes/pages/category.html', context={
-        'recipes': recipes,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
         'title': f'{recipes[0].category.name}  - Category'
     })
 
@@ -80,9 +67,13 @@ def search(request):
         is_published=True
     ).order_by('-id')
 
+    page_obj, pagination_range = make_pagination(request, receitas, 3)
+
     # A página só será renderisada se a condição acima for falsa
     return render(request, 'recipes/pages/search.html', {
         'page_title': f'Search for {search_term}',
         'search_term': search_term,
-        'recipes': receitas,
+        'recipes': page_obj,
+        'pagination_range': pagination_range,
+        'additional_url_query': f'&q={search_term}',
     })
--- utils/pagination.py
+++ utils/pagination.py
@@ -1,4 +1,5 @@
 import math
+from django.core.paginator import Paginator
 
 
 def make_pagination_range(
@@ -33,3 +34,23 @@ def make_pagination_range(
         'first_page_out_of_range': current_page > middle_range,
         'last_page_out_of_range': stop_range < total_pages,
     }
+
+
+def make_pagination(request, queryset, per_page):
+    # A variável recebe uma query string chamada 'page' caso nela não exista nada retornara o valor 1 # noqa
+    try:
+        current_page = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+    # A variável recebe um objeto paginator que irá retornar os objetos 'recipes' de acordo com a quantidade definida no segundo parâmetro # noqa
+    paginator = Paginator(queryset, per_page)
+    # A variável recebe o objeto que irá retornar uma página, como no parâmetro está sendo passado o 'current_page' a página exibida será determinada pelo valor da query string nele atribuida # noqa
+    page_obj = paginator.get_page(current_page)
+
+    pagination_range = make_pagination_range(
+        paginator.page_range,
+        4,
+        current_page
+    )
+
+    return page_obj, pagination_range
