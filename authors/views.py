7bb2ac8d896b360b568025a8444fd71ca3f498d7
gabrielkemerson <gabrielkemersony@gmail.com>
2023-10-25 15:28:25 -0300

    Alterando CSS dos formulários

--- authors/views.py
+++ authors/views.py
@@ -6,21 +6,25 @@ from django.http import Http404
 
 
 def register_view(request):
+    # Recebe o formulário que foi armazenado na session caso não exista nada receberá o None retornado # noqa
     register_form_data = request.session.get('register_form_data', None)
+    # Esta variável recebe um objeto de formulário que foi criado no arquivo form.py deste app. E este objeto recebe os dados da variável a cima # noqa
     form = RegisterForm(register_form_data)
-
+    # Retorna o template de registro e as informações salvas na session no contexto # noqa
     return render(request, 'authors/pages/register_view.html', {
         'form': form,
     })
 
 
 def register_create(request):
-
+    # Se o usuário tentar acessar esta view diretamento por um methodo que não seja POST isso retornará um erro 404 # noqa
     if not request.POST:
         raise Http404()
-
+    # Esta constante recebe os dados do formulário que foram passados na página HTML e ela foi definida como receptora desses dados na Action fo formulário da página # noqa
     POST = request.POST
+    # Aqui criamos uma nova chave dentro da nossa session chamada de register_form_data, e essa chave irá receber todos os dados da constante POST # noqa
     request.session['register_form_data'] = POST
     form = RegisterForm(POST)
 
+    # Aqui á view redireciona para a view a cima "register_view" e na outra view esses dados da session serão recebidos # noqa
     return redirect('authors:register')
