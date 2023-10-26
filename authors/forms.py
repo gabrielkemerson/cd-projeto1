aef96a3f9055104a52a3b870a4444c8e40a5b1c8
gabrielkemerson <gabrielkemersony@gmail.com>
2023-10-26 16:42:00 -0300

    Validando campos do formulário

--- authors/forms.py
+++ authors/forms.py
@@ -1,11 +1,18 @@
 from django import forms
 from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
+
+
+# Nesta função receberemos um campo, o widget que será alterado e o novo valor deste widget # noqa
+def add_attr(field, attr_name, attr_new_val):
+    existing = field.widget.attrs.get(attr_name, '')
+    field.widget.attrs[attr_name] = f'{existing} {attr_new_val}'.strip()
 
 
 # esta função recebe um campo e uma string pare redefinir o placeholder
 # Neste caso não re reescrevemos um field, apenas adicionamos imformações a ele
 def add_placeholder(field, placeholder_val):
-    field.widget.attrs['placeholder'] = placeholder_val
+    add_attr(field, 'placeholder', placeholder_val)
 
 
 class RegisterForm(forms.ModelForm):
@@ -13,6 +20,7 @@ class RegisterForm(forms.ModelForm):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         # Aqui é ultilizado a função para adicionar um texto ao placeholder
+
         add_placeholder(self.fields['last_name'], 'Sobre nome')
 
     # Sub-escrições
@@ -55,8 +63,9 @@ class RegisterForm(forms.ModelForm):
         widget=forms.PasswordInput(attrs={
             'placeholder': 'Confirme sua senha'
         }),
+
         error_messages={
-            'required': '* Obrigatório'
+            'required': '*obrigatório',
         }
     )
 
@@ -83,3 +92,29 @@ class RegisterForm(forms.ModelForm):
                 'placeholder': 'Senha'
             })
         }
+
+    def clean_password(self):
+        data = self.cleaned_data.get('password')
+
+        if 'atenção' in data:
+
+            raise ValidationError(
+                'Não digite %(value)s na sua senha!',
+                code='invalid',
+                params={'value': '"Atenção"'}
+            )
+
+        return data
+
+    def clean_username(self):
+        data = self.cleaned_data.get('username')
+
+        if 'robertinho' in data:
+
+            raise ValidationError(
+                'Seu nome de usuário não pode conter %(value)s',
+                code='invalid',
+                params={'value': '"Robertinho"'}
+            )
+
+        return data
